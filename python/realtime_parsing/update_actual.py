1463d360d3bf71b5136343e5461857471d88349a
maxifom <vominyh@yandex.ru>
2019-05-10 12:10:23 +0200

    update actual fix (holidays)

--- python/realtime_parsing/update_actual.py
+++ python/realtime_parsing/update_actual.py
@@ -1,6 +1,7 @@
 """
     Gets actual price on real-time predictions to calculate accuracy
 """
+from datetime import timedelta, datetime
 
 from models import Prediction, Price, fn
 
@@ -12,15 +13,21 @@ def update_predictions():
         # Monday = 0, Sunday = 6
         # If Friday or Saturday or Sunday then search for Monday
         if 4 <= p.time.weekday() <= 6:
-            price = Price.select(Price.current).where(
-                (Price.time > p.time) & (fn.WEEKDAY(Price.time) == 0) & (Price.company == p.company)).order_by(
-                Price.id.desc()).limit(1)
+            for i in [0, 1, 2, 3, 4, 5, 6]:
+                price = Price.select(Price.current).where(
+                    (Price.time > p.time) & (fn.WEEKDAY(Price.time) == i) & (Price.company == p.company)).order_by(
+                    Price.id.desc()).limit(1)
+                if len(price) > 0 or (p.time + timedelta(days=i)) > datetime.now():
+                    break
         else:
-            weekday = (p.time.weekday() + 1) % 7
-            price = Price.select(Price.current).where(
-                (Price.time > p.time) & (
-                        fn.WEEKDAY(Price.time) == weekday) & (Price.company == p.company)).order_by(
-                Price.id.desc()).limit(1)
+            for i in [1, 2, 3, 4, 5, 6]:
+                weekday = (p.time.weekday() + i) % 7
+                price = Price.select(Price.current).where(
+                    (Price.time > p.time) & (
+                            fn.WEEKDAY(Price.time) == weekday) & (Price.company == p.company)).order_by(
+                    Price.id.desc()).limit(1)
+                if len(price) > 0 or p.time + timedelta(days=i) > datetime.now():
+                    break
         price = price[0].current if len(price) > 0 else None
         if price is not None:
             p.actual = price
